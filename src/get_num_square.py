f62a46f273bdefd3a41fdb355d155579e3d41b21
Akhil KM <akhilu011@gmail.com>
2023-05-13 17:09:35 +0530

    updated

--- src/get_num_square.py
+++ src/get_num_square.py
@@ -2,13 +2,6 @@
 import os
 import boto3
 
-s3_client = boto3.client('s3')
-
-objects = s3_client.list_objects_v2(Bucket='stackvidhya')
-
-for obj in objects['Contents']:
-    print(obj['Key'])
-
 # get the input and convert it to int
 num = os.environ.get("INPUT_NUM")
 if num:
@@ -18,6 +11,22 @@ if num:
         exit('ERROR: the INPUT_NUM provided ("{}") is not an integer'.format(num))
 else:
     num = 1
+
+s3_client = boto3.client('s3')
+
+
+my_config = Config(
+    region_name = 'ap-southeast-1',
+    signature_version = 'v4',
+    retries = {
+        'max_attempts': 10,
+        'mode': 'standard'
+    }
+)
+objects = s3_client.list_objects_v2(Bucket='cloudbank-terraform-backend',  config=my_config)
+
+for obj in objects['Contents']:
+    print(obj['Key'])
 
 # to set output, print to shell in following syntax
 print(f"::set-output name=num_squared::{num ** 2}")
