5f6eb5924d2dc9829e5b774acc41d1cf70028b89
Akhil KM <akhilu011@gmail.com>
2023-05-16 14:29:08 +0530

    updated

--- ci/run_ssm.py
+++ ci/run_ssm.py
@@ -8,20 +8,16 @@ client = boto3.client('ssm')
 region = os.getenv('AWS_REGION')
 branch = os.getenv('GITHUB_REF_NAME')
 instance_id = os.getenv('INSTANCE_ID')
-print("environment variables")
-print(branch)
-print(instance_id)
-exit(1)
 
 try:
     response = client.send_command(
         # Targets=[{"Key":"tag:deploy","Values":["ssm"]}],
-        InstanceIds=[],
+        InstanceIds=[instance_id],
         DocumentName='AWS-RunShellScript',
         TimeoutSeconds=600,
         Comment='deploy',
 
-        Parameters={"workingDirectory":["/home/ec2-user/"],"commands":["#!/bin/bash","su darren","source /home/darren/scripts/.env","echo \"Working Environment = $UMI_ENV\"","cd /opt/mer/blush","git fetch --all --tags","echo \"branch is \""+branch,"git checkout "+branch,"git branch","npm ci","npm run build","npm run test","exit 0"],"workingDirectory":["/home/darren/"]},
+        Parameters={"workingDirectory":["/home/darren/"],"commands":["#!/bin/bash","su darren","source /home/darren/scripts/.env","echo \"Working Environment = $UMI_ENV\"","cd /opt/mer/blush","git fetch --all --tags","echo \"branch is \""+branch,"git checkout "+branch,"git branch","npm ci","npm run build","npm run test","exit 0"]},
         # Parameters={"commands":["ls -al; sleep 60;  echo \"hello\""],"workingDirectory":["/home/ec2-user/"]},
         CloudWatchOutputConfig={'CloudWatchLogGroupName': 'akhil-test', 'CloudWatchOutputEnabled': True}
     )
@@ -33,7 +29,7 @@ print("Started SSM command with commandid:", response["Command"]["CommandId"])
 
 print("\n\nlogs....\n\n")
 time.sleep(2)
-p = subprocess.Popen("awslogs get akhil-test --region ap-southeast-1 ALL --watch", shell=True)
+p = subprocess.Popen("awslogs get akhil-test --region {} ALL --watch".format(region), shell=True)
 while True:
     time.sleep(10)
     status = client.list_commands(CommandId=response["Command"]["CommandId"])['Commands'][0]["Status"]
